374641e83cc9272de22ef4e03845f6623f61e0d7
Arindra_Production <58177634+arindraproduction@users.noreply.github.com>
2019-11-25 21:08:27 +0700

    halaman basic portofolio.php

---
+++ portofolio.php
@@ -0,0 +1,83 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!-- Slider -->
+<div class="slider">
+	<ul class="slides">
+		<li>
+			<img src="<?php echo base_url('asset/slider/mgm.jpg'); ?>">
+			<div class="caption left-align">
+				<h3>Portofolio</h3>
+				<h5 class="light grey-text text-lighten-3">Menerima Jasa Pembuatan Video Dengan Yang Professional Di Bidangnya</h5>
+			</div>
+		</li>
+	</ul>
+</div>
+<!-- Akhir Slider -->
+<!-- portofolio video event -->
+<section id="services" class="services">
+	<div class="container">
+		<div class="row">
+			<h1 class="light center grey-text text-darken-3">Portofolio Video Event</h1>
+			<div class="col m4 s12">
+				<!-- card 1 -->
+				<div class="youtube-player" data-id="phNUlXufaNE"></div>
+			</div>
+			<!-- akhir card 1 -->
+			<!-- card 2 -->
+			<div class="col m4 s12">
+				<div class="youtube-player" data-id="EiuJxDvv6k0"></div>
+			</div>
+			<!-- akhir card 2 -->
+			<!-- card 3 -->
+			<div class="col m4 s12">
+				<div class="youtube-player" data-id="XCgLEeAUU7w"></div>
+			</div>
+			<!-- akhir card 3 -->
+		</div>
+	</div>
+	<!-- card 2 -->
+	<div class="container">
+		<div class="row">
+			<div class="col m4 s12">
+				<!-- card 1 -->
+				<div class="youtube-player" data-id="v8xLtNM3H7M"></div>
+			</div>
+			<!-- akhir 1 -->
+			<!-- card 2 -->
+			<div class="col m4 s12">
+				<div class="youtube-player" data-id="xgd0FFXIr1w"></div>
+			</div>
+			<!-- akhir card 2 -->
+			<!-- card 3 -->
+			<div class="col m4 s12">
+				<div class="youtube-player" data-id="CROw6ZSzvRg"></div>
+			</div>
+			<!-- akhir card 3 -->
+		</div>
+
+	</div>
+	<!-- card 3 -->
+	<div class="container">
+		<div class="row">
+			<div class="col m4 s12">
+				<!-- card 1 -->
+				<div class="youtube-player" data-id="22jtxw7CzqM"></div>
+			</div>
+			<!-- akhir 1 -->
+			<!-- card 2 -->
+			<div class="col m4 s12">
+				<div class="youtube-player" data-id="lZSoRvp3_do"></div>
+			</div>
+			<!-- akhir card 2 -->
+			<!-- card 3 -->
+			<div class="col m4 s12">
+				<div class="youtube-player" data-id="kLrSkxSSE5g"></div>
+			</div>
+			<!-- akhir card 3 -->
+		</div>
+
+	</div>
+	</div>
+</section>
+<!-- Akhir Portofolio -->
